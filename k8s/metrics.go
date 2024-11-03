0c15f5f73b2447b808b96a2085c59cf6e6daded5
steugen <p.gnomme@gmail.com>
2024-11-04 00:49:06 +0400

    k8s/metrics.go dependencies changed

--- k8s/metrics.go
+++ k8s/metrics.go
@@ -4,10 +4,23 @@ import (
     "context"
     "k8s.io/client-go/kubernetes"
     "k8s.io/metrics/pkg/apis/metrics/v1beta1"
+    "k8s.io/client-go/rest"
+    metricsclientset "k8s.io/metrics/pkg/client/clientset/versioned"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func GetNodeMetrics(clientset *kubernetes.Clientset) (*v1beta1.NodeMetricsList, error) {
-    metricsClient := clientset.MetricsV1beta1()
-    return metricsClient.NodeMetricses().List(context.Background(), v1.ListOptions{})
-}
+    
+    config, err := rest.InClusterConfig()
+    
+    if err != nil {
+        return nil, err
+    }
 
+    metricsClient, err := metricsclientset.NewForConfig(config)
+    if err != nil {
+        return nil, err
+    }
+
+    return metricsClient.MetricsV1beta1().NodeMetricses().List(context.Background(), metav1.ListOptions{})
+}
